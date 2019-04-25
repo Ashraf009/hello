4818ce9f7fb98f2f7e3f3c752dd811f70962049c
Administrator <Administrator@192.168.0.8>
2019-04-25 22:46:30 +0200

    Pushing 3 Tests

--- SeleniumTest/src/test/test.java
+++ SeleniumTest/src/test/test.java
@@ -4,16 +4,37 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class test {
-
+    
     public static void main(String[] args) {
-    	System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://www.google.com/");
-        WebElement element = driver.findElement(By.name("q"));
-        element.sendKeys("mysql excel 2014");
-        element.submit();
+    	//System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+        //WebDriver driver = new ChromeDriver();
+        //driver.get("http://www.google.com/");
+        //WebElement element = driver.findElement(By.name("q"));
+        //element.sendKeys("mysql excel 2014");
+        //element.submit();
     }
+
+
+@Test
+public void Test1()
+{
+	 Assert.assertEquals(10, 10);
+}
+
+@Test
+public void Test2()
+{
+	 Assert.assertEquals(20, 10);
+}
+
+@Test
+public void Test3()
+{
+	 Assert.assertEquals(10, 10);
+}
 
 }
