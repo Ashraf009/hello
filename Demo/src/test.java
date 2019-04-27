7469f4d266ea987706fc70e6abe097db00e01f11
Administrator <Administrator@192.168.0.8>
2019-04-28 00:03:40 +0200

    kkk

---
+++ Demo/src/test.java
@@ -0,0 +1,17 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Test
+	public void Test1()
+	{
+		System.out.print("pakiskkkn");
+	}
+	
+}
